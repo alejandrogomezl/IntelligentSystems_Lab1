3502f0a4ac90b64f7b8948cfbb9f790b78a1993e
Alejandro Gomez Lozano <alejandrogomezlozano@hotmail.com>
2024-10-16 19:50:31 +0200

    DFS added

--- Solution/main.py
+++ Solution/main.py
@@ -3,24 +3,20 @@ from search import Search
 from problem import Problem
 from graph import GraphVisualizer
 
-# Cargar el archivo JSON
 file = "./problems/small/calle_agustina_aroca_albacete_250_0.json"
 
-# Leer el archivo JSON y obtener el grafo y las posiciones
 json_reader = JsonReader(file)
 graph = json_reader.get_graph()
 positions = json_reader.get_positions()
 initial_state = json_reader.initial
 goal_state = json_reader.final
 
-# Definir el problema con estado inicial y objetivo
 problem = Problem(initial=initial_state, goal=goal_state, graph=graph)
 
-# Ejecutar el algoritmo de búsqueda
 search_instance = Search(problem)
 path = search_instance.bfs()
+path = search_instance.dfs()
 
-# Mostrar el resultado
 if path:
     print(f"Camino encontrado: {path}")
     visualizer = GraphVisualizer(graph, positions, initial_state, goal_state)
--- Solution/search.py
+++ Solution/search.py
@@ -38,3 +38,41 @@ class Search:
                         print(f"Nodes generated: {self.nodes_generated}")
                         print(f"Nodes explored: {self.nodes_explored}")
                         return child_node.path()
+                    
+
+    def dfs(self):
+        start_time = time.perf_counter()
+        open_list = [Node(self.problem.initial)]
+        explored = set()
+        self.nodes_generated = 1
+        self.nodes_explored = 0
+
+        while open_list:
+            node = open_list.pop()  # LIFO stack
+            self.nodes_explored += 1
+
+            if self.problem.goal_test(node.state):
+                end_time = time.perf_counter()
+                execution_time = end_time - start_time
+                print(f"Execution time: {execution_time:.6f} seconds")
+                print(f"Nodes generated: {self.nodes_generated}")
+                print(f"Nodes explored: {self.nodes_explored}")
+                return node.path()
+
+            explored.add(node.state)
+
+            # for action in sorted(self.problem.actions(node.state), reverse=True):
+            for action in self.problem.actions(node.state):
+                child_state = self.problem.result(node.state, action)
+                if child_state not in explored and all(front_node.state != child_state for front_node in open_list):
+                    child_node = Node(child_state, node, action, node.path_cost + self.problem.step_cost(node.state, action))
+                    open_list.append(child_node)
+                    self.nodes_generated += 1
+
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+        print(f"Execution time: {execution_time:.6f} seconds")
+        print(f"Nodes generated: {self.nodes_generated}")
+        print(f"Nodes explored: {self.nodes_explored}")
+        return None
+    
